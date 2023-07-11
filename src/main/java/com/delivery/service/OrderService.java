ca09723acc6e7d4d47678a5b317fbf72398f71a6
Taritaleite <taritaleite@gmail.com>
2023-07-10 23:03:20 -0300

    Desafio 1 - Sistema Delivery

--- src/main/java/com/delivery/service/OrderService.java
+++ src/main/java/com/delivery/service/OrderService.java
@@ -2,16 +2,14 @@ package com.delivery.service;
 
 
 import com.delivery.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
+    @Autowired
     private ShippingService shippingService;
-
-    public OrderService(ShippingService shippingService) {
-        this.shippingService = shippingService;
-    }
 
     public double totalValue (Order order) {
         return order.getValueBasic() - (order.getValueBasic() * order.getDiscout()/100)
--- src/main/java/com/delivery/sistemadelivery/SistemadeliveryApplication.java
+++ src/main/java/com/delivery/sistemadelivery/SistemadeliveryApplication.java
@@ -3,6 +3,9 @@ package com.delivery.sistemadelivery;
 import com.delivery.entities.Order;
 import com.delivery.service.OrderService;
 import com.delivery.service.ShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,31 +14,18 @@ import java.util.Scanner;
 
 @SpringBootApplication
 @ComponentScan({"com.delivery"})
-public class SistemadeliveryApplication {
+public class SistemadeliveryApplication implements CommandLineRunner{
 
+    @Autowired
+    private OrderService orderService;
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        SpringApplication.run(SistemadeliveryApplication.class, args);
+    }
 
-        System.out.print("Enter your order code: ");
-        Integer code = sc.nextInt();
-
-        System.out.print("Enter the base amount of your order: ");
-        Double basic = sc.nextDouble();
-
-        System.out.print("Enter the discount amount for your order: ");
-        Double discout = sc.nextDouble();
-
-        Order order = new Order(code, basic, discout);
-
-        ShippingService shippingService = new ShippingService();
-        OrderService orderService = new OrderService(shippingService);
-
-        double valueTotal = orderService.totalValue(order);
-
-        System.out.println("Pedido Código: " + code);
-        System.out.printf("Valor Total: R$ %.2f%n", valueTotal);
-
-        sc.close();
+    @Override
+    public void run(String... args) throws Exception {
+        Order order = new Order(1034, 150.00, 20.0);
+        System.out.println("Pedido Codigo: " + order.getCode());
+        System.out.printf("Valor Total: R$ %.2f%n", orderService.totalValue(order));
     }
 }
